5615255af4c2d24fa1e2c1a9adf02c8dc0d04096
ioangheraszim <gijohny17@gmail.com>
2023-07-21 15:42:38 +0100

    further testing and trying to load images

--- src/components/RightSide.jsx
+++ src/components/RightSide.jsx
@@ -3,8 +3,8 @@ import React from "react";
 const RightSide = () => {
   return (
     <div className='right-side'>
-      <img className='desktop-illustration' src="./react-newsletter-fe/src/assets/images/illustration-sign-up-desktop.svg" alt="desktop illustration" />
-      <img className='mobile-illustration' src="./react-newsletter-fe/src/assets/images/illustration-sign-up-mobile.svg" alt="mobile illustration" />
+      <img className='desktop-illustration' src="./react-newsletter-fe/public/images/illustration-sign-up-desktop.svg" alt="desktop illustration" />
+      <img className='mobile-illustration' src="./react-newsletter-fe/public/images/illustration-sign-up-mobile.svg" alt="mobile illustration" />
     </div>
   );
 };
