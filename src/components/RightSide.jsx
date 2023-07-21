b710edfb9e0df08d0c7be57cd0e15b5610729db8
ioangheraszim <gijohny17@gmail.com>
2023-07-21 20:19:31 +0100

    updated images path it should work properly

--- src/components/RightSide.jsx
+++ src/components/RightSide.jsx
@@ -3,8 +3,8 @@ import React from "react";
 const RightSide = () => {
   return (
     <div className='right-side'>
-      <img className='desktop-illustration' src="/images/illustration-sign-up-desktop.svg" alt="desktop illustration" />
-      <img className='mobile-illustration' src="/images/illustration-sign-up-mobile.svg" alt="mobile illustration" />
+      <img className='desktop-illustration' src="./images/illustration-sign-up-desktop.svg" alt="desktop illustration" />
+      <img className='mobile-illustration' src="./images/illustration-sign-up-mobile.svg" alt="mobile illustration" />
     </div>
   );
 };
