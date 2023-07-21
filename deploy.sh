960d91b48400bd0591f02a1a977cd7bced2e1337
ioangheraszim <gijohny17@gmail.com>
2023-07-21 20:07:22 +0100

    update new way to deploy i guess?

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#place .nojekyll to bypass Jekyll
+echo > .nojekyll
+
+#if you are deploying to a custom domain
+# echo 'www.example.com' > CNAME
+
+git init 
+git checkout -B main
+git add -A
+git commit -m 'deploy'
+#if you are deploying to https://<USERNAME>.github.io
+#git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+
+#if you are deploying to https https://<USERNAME>.github.io/<REPO>
+#git push -f git@github.com:ioangheraszim/react-newsletter-fe
+
+cd -
